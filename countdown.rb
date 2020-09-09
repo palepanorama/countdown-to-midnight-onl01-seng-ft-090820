6cb374cf65615fc8c463750b18b5b4d600e6343f
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 14:09:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,16 @@
 #write your code here
 
 
-def countdown
-  counter = 11
-  while counter > 1 
-    counter -= 1 
-    puts "#{counter} SECONDS!"
+def countdown(num)
+  while num > 0 
+    puts "#{num} SECOND(S)!"
+    num -= 1 
   end
   
   puts "HAPPY NEW YEAR!"
 end
 
-countdown 
+
 
 def countdown_with_sleep
   counter = 11
