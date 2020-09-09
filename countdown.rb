ed406f179fde78715059e31fcd256dab0eba442e
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-09 14:07:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
+
 def countdown
+  counter = 11
+  while counter > 1 
+    counter -= 1 
+    puts "#{counter} SECONDS!"
+  end
+  
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown 
+
+def countdown_with_sleep
+  counter = 11
